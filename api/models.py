ed6dfb7aca86db837f71111dd1f7ff0af5ff3956
RitvikDayal-github <ritvikr1605@gmail.com>
2020-09-04 17:53:08 +0530

    made add,list,delete,update apis

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=150)
+    task_detail = models.TextField(blank=True, null=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    schedule_date = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return 'Task: {}'.format(self.task)
